ac5ab610785ee77772a780be73d4bd37d3f98e50
Alexander John <96aljohn@gmail.com>
2025-02-11 19:38:07 -0800

    update navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { BrowserRouter, Link, Route, Routes , NavLink} from "react-router";
+
 import Navbar from "./Navbar";
+import Home from "./Home";
+import Javascript from "./Javascript";
+import Css from "./Css";
+import Html from "./Html";
 
 function App() {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <BrowserRouter>
+            <header>
+                <nav>
+                    <nav id="desktop-nav">
+                        <ul class="nav-options">
+                            <li class="btn flex-center">
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li class="btn flex-center">
+                                <NavLink to="javascript">JavaScript</NavLink>
+                            </li>
+                            <li class="btn flex-center">
+                                <NavLink to="html">HTML</NavLink>
+                            </li>
+                            <li class="btn flex-center">
+                                <NavLink to="css">CSS</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="javascript" element={<Javascript />} />
+                <Route path="html" element={<Html />} />
+                <Route path="css" element={<Css />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
