51614c2a2d3b1cb302ecd99a7414c19227c1f58a
Alexander John <96aljohn@gmail.com>
2025-02-13 12:16:36 -0800

    add grid system for navigation assistance

---
+++ src/Grid.jsx
@@ -0,0 +1,41 @@
+const Grid = () => {
+    return (
+        <div className="grid-container">
+            <p>
+                <h1>Multiple Choice</h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                dolor assumenda, itaque nesciunt alias voluptatum nobis
+                blanditiis eos officiis aperiam earum eum vel quas odio optio,
+                distinctio ab sunt unde incidunt ipsum omnis amet magnam
+                accusantium aut! Excepturi, cupiditate iusto!
+            </p>
+            <p>
+                <h1>Drag and Drop</h1>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Necessitatibus veniam ducimus eligendi nihil, cumque ab eveniet
+                modi architecto quidem, non odit saepe facere voluptas esse
+                mollitia illo fuga exercitationem id dicta iusto eaque numquam
+                quaerat ad! Fugit velit beatae laudantium.
+            </p>
+            <p>
+                <h1>True or False</h1>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Necessitatibus veniam ducimus eligendi nihil, cumque ab eveniet
+                modi architecto quidem, non odit saepe facere voluptas esse
+                mollitia illo fuga exercitationem id dicta iusto eaque numquam
+                quaerat ad! Fugit velit beatae laudantium.
+            </p>
+            <p>
+                <h1>Coding Challenges</h1>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Necessitatibus veniam ducimus eligendi nihil, cumque ab eveniet
+                modi architecto quidem, non odit saepe facere voluptas esse
+                mollitia illo fuga exercitationem id dicta iusto eaque numquam
+                quaerat ad! Fugit velit beatae laudantium.
+            </p>
+        </div>
+    );
+};
+
+
+export default Grid;
--- src/Html.jsx
+++ src/Html.jsx
@@ -1,4 +1,5 @@
 import SecNavbar from "./SecNavbar";
+import Grid from "./Grid";
 
 export default function Html() {
     return (
@@ -6,29 +7,7 @@ export default function Html() {
             <SecNavbar />
             <div className="html">
                 <h2>HTML</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Rerum dolor assumenda, itaque nesciunt alias voluptatum
-                    nobis blanditiis eos officiis aperiam earum eum vel quas
-                    odio optio, distinctio ab sunt unde incidunt ipsum omnis
-                    amet magnam accusantium aut! Excepturi, cupiditate iusto!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Necessitatibus veniam ducimus eligendi nihil, cumque ab
-                    eveniet modi architecto quidem, non odit saepe facere
-                    voluptas esse mollitia illo fuga exercitationem id dicta
-                    iusto eaque numquam quaerat ad! Fugit velit beatae
-                    laudantium.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Necessitatibus veniam ducimus eligendi nihil, cumque ab
-                    eveniet modi architecto quidem, non odit saepe facere
-                    voluptas esse mollitia illo fuga exercitationem id dicta
-                    iusto eaque numquam quaerat ad! Fugit velit beatae
-                    laudantium.
-                </p>
+                <Grid />
             </div>
         </>
     );
