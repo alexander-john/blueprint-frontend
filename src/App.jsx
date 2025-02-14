94f2d9b8987c6bed7d63046c532591359e91f945
Alexander John <96aljohn@gmail.com>
2025-02-13 20:33:15 -0800

    create multiple choice route

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ import Javascript from "./pages/Javascript";
 import Css from "./pages/Css";
 import Html from "./pages/Html";
 import Navbar from "./Navbar";
+import MultipleChoice from "./pages/MultipleChoice";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Navbar />}>
             <Route path="/" element={<Home />} />
             <Route path="javascript" element={<Javascript />}>
-                <Route path="multiple-choice" />
+                <Route path="multiple-choice" element={<MultipleChoice />}/>
             </Route>
             <Route path="html" element={<Html />} />
             <Route path="css" element={<Css />} />
---
+++ src/pages/MultipleChoice.jsx
@@ -0,0 +1,9 @@
+const MultipleChoice = () => {
+    return (
+        <>
+        <h1>Hello Multiple Choice!</h1>
+        </>
+    )
+}
+
+export default MultipleChoice;
